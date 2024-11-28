4057745eba6d44825b6c21ec400348d8f91d43b4
Kobbe <korbanj1108@gmail.com>
2024-11-28 16:10:28 +0100

    FE

--- src/Components/Home/Content/Content.tsx
+++ src/Components/Home/Content/Content.tsx
@@ -1,7 +1,19 @@
 import styles from "./Content.module.scss";
 import { GalleryPage } from "./GalleryPage/GalleryPage";
 
-export function Content() {
+export function Content({
+  setIsHomePage,
+  setIsUsersPage,
+  setIsGalleryPage,
+  setIsFoldersPage,
+  setIsPostsPage,
+}): {
+  setIsHomePage: (isHomePage: boolean) => void;
+  setIsUsersPage: (isUsersPage: boolean) => void;
+  setIsGalleryPage: (isGalleryPage: boolean) => void;
+  setIsFoldersPage: (isFoldersPage: boolean) => void;
+  setIsPostsPage: (isPostsPage: boolean) => void;
+} {
   return (
     <div id={styles.content}>
       <GalleryPage />
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,17 +1,32 @@
+import styles from "./Home.module.scss";
+import { useState } from "react";
+
 import { Content } from "./Content/Content";
 import { FilterBar } from "./FilterBar/FilterBar";
-import styles from "./Home.module.scss";
 import { NavBar } from "./NavBar/NavBar";
 import { TopBar } from "./TopBar/TopBar";
 
 export function Home() {
+  const [isHomePage, setIsHomePage] = useState(true);
+  const [isUsersPage, setIsUserPage] = useState(false);
+  const [isGalleryPage, setIsGalleryPage] = useState(false);
+  const [isFoldersPage, setIsFoldersPage] = useState(false);
+  const [isPostsPage, setIsPostsPage] = useState(false);
+
   return (
     <div id={styles.mainWindow}>
       <NavBar />
       <div id={styles.content}>
         <TopBar />
         <FilterBar />
-        <Content />
+        <Content
+          setIsHomePage={setIsHomePage}
+          setIsUsersPage={setIsUserPage}
+          setIsGalleryPage={setIsGalleryPage}
+          settIsFoldersPage={setIsFoldersPage}
+          setIsPostsPage={setIsPostsPage}
+        />
+        {[isHomePage, isUsersPage, isGalleryPage, isFoldersPage, isPostsPage]}
       </div>
     </div>
   );
