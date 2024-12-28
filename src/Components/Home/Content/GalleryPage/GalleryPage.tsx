8a2045af37e38614196e2433cbb88942362ee88a
BacaKamil <kamil.baca@outlook.com>
2024-12-29 00:58:33 +0100

    add notifications on gallery page and folder page

--- src/Components/Home/Content/GalleryPage/GalleryPage.tsx
+++ src/Components/Home/Content/GalleryPage/GalleryPage.tsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { Item } from "../Item/Item";
 import styles from "./GalleryPage.module.scss";
 import api from "../../../../ApiConfig/ApiConfig";
+import Notifications from '../Notifications/Notifications';
 
 interface ImageData {
   id: number;
@@ -17,6 +18,8 @@ export function GalleryPage() {
   const [images, setImages] = useState<ImageData[]>([]);
   const [checkedItems, setCheckedItems] = useState<number[]>([]);
   const removeCheckboxesGallery = location.state?.removeCheckboxesGallery || false;
+  const [message, setMessage] = useState<string>("");
+  const [messageType, setMessageType] = useState<"success" | "error" | "warning" | null>(null);
 
   useEffect(() => {
     fetchImages();
@@ -45,21 +48,34 @@ export function GalleryPage() {
     }
   };
 
-  const handleGalleryAdd = async () => {
-    fileInputRef.current?.click();
-
-    if (fileInputRef.current && fileInputRef.current.files) {
+  const handleImageAdd = async () => {
+    await fileInputRef.current?.click();
+    
+    if (!fileInputRef.current || !fileInputRef.current.files?.length) return;
+  
+    try {
       const file = fileInputRef.current.files[0];
       const formData = new FormData();
       formData.append("file", file);
 
       await api.post("/upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
+        headers: { "Content-Type": "multipart/form-data" },
       });
-
+      setMessage("Image added successfully!");
+      setMessageType("success");
       fetchImages();
+      setTimeout(() => {
+        setMessage("");
+        setMessageType(null);
+      }, 3000);
+    }
+    catch (error) {
+      setMessage("Error adding image(s)! " + error);
+      setMessageType("error");
+      setTimeout(() => {
+        setMessage("");
+        setMessageType(null);
+      }, 3000);
     }
   };
 
@@ -70,26 +86,45 @@ export function GalleryPage() {
 
     setCheckedItems(prevCheckedItems =>
       prevCheckedItems.includes(id)
-        ? prevCheckedItems.filter(item => item !== id)
+         ? prevCheckedItems.filter(item => item !== id)
         : [...prevCheckedItems, id]
     );
   };
 
   const removeImage = async () => {
-    for (const id of checkedItems) {
-      await api.delete(`/image/${id}`);
-      navigate("/gallery", { state: { removeCheckboxesGallery: false } });
-      fetchImages();
+    try {
+      for (const id of checkedItems) {
+        await api.delete(`/image/${id}`);
+        navigate("/gallery", { state: { removeCheckboxesGallery: false } });
+        fetchImages();
+      }
+
+      setMessage("Image(s) removed successfully!");
+        setMessageType("success");
+        setTimeout(() => {
+          setMessage("");
+          setMessageType(null);
+        }, 3000);
+    }
+    catch (error) {
+      setMessage("Error removing image(s)! " + error);
+      setMessageType("error");
+      setTimeout(() => {
+        setMessage("");
+        setMessageType(null);
+      }, 3000);
     }
   };
 
   if (location.state?.triggerAddGallery) {
-    handleGalleryAdd();
+    handleImageAdd();
     location.state.triggerAddGallery = false;
   }
 
   return (
     <div className={styles.galleryPage}>
+
+      <Notifications messageType={messageType} message={message} />
 
       {removeCheckboxesGallery ? (
         <button className={styles.removeImageBtn} onClick={removeImage} disabled={checkedItems.length === 0}>Remove</button> 
@@ -118,7 +153,7 @@ export function GalleryPage() {
       <input
         type="file"
         ref={fileInputRef}
-        onChange={handleGalleryAdd}
+        onChange={handleImageAdd}
         style={{ display: "none" }}
       />
 
---
+++ src/Components/Home/Content/Notifications/Notifications.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "./Notifications.module.scss";
+
+interface NotificationsProps {
+  messageType: "success" | "error" | "warning" | null;
+  message: string;
+}
+
+const Notifications: React.FC<NotificationsProps> = ({ messageType, message }) => {
+
+  return (
+    <div className={styles.notifications}>
+      {message && (
+        <div className={`${styles.message} ${messageType ? styles[messageType] : ""}`}>
+          {message}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Notifications;
