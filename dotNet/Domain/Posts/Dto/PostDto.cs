642d4efcd98a5aefed89b7806d0942ab545f7b39
BacaKamil <kamil.baca@outlook.com>
2025-01-05 14:43:50 +0100

    change navBarBtn

--- dotNet/Domain/Posts/Dto/PostDto.cs
+++ dotNet/Domain/Posts/Dto/PostDto.cs
@@ -1,6 +1,7 @@
 namespace Domain.Posts.Dto;
 
 public record PostDto(
+    int Id,
     string Title,
     string Content,
     string Author);
--- src/Components/Home/Content/PostsPage/PostsPage.tsx
+++ src/Components/Home/Content/PostsPage/PostsPage.tsx
@@ -1,9 +1,52 @@
+import { useState, useEffect } from "react";
 import styles from "./PostsPage.module.scss";
+import api from "../../../../ApiConfig/ApiConfig";
+import Notifications from '../../../Notifications/Notifications';
 
-export function PostsPage() {
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+}
+
+export function UsersPage() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [message, setMessage] = useState<string>("");
+  const [messageType, setMessageType] = useState<"success" | "error" | "warning" | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get("/posts");
+        setPosts(response.data);
+      } catch (error) {
+        setMessage("Error fetching posts: " + error);
+        setMessageType("error");
+        setTimeout(() => {
+          setMessage("");
+          setMessageType(null);
+        }, 3000);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div className={styles.postsPage}>
-      <h1>Posts Page</h1>
+    <div className={styles.usersPage}>
+      <Notifications messageType={messageType} message={message} />
+      <div className={styles.userList}>
+        {posts.map((post) => (
+          <div className={styles.userCard}>
+            <div className={styles.userName}>
+              <strong>{post.title}</strong>
+            </div>
+            <div className={styles.userEmail}>
+              <strong>{post.content}</strong>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
