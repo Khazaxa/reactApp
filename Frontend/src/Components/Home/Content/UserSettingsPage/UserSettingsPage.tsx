045e2dec41fc63ce573d705574e7b4b6368bc3e8
BacaKamil <kamil.baca@outlook.com>
2025-01-30 22:25:35 +0100

    fix user edit

--- Frontend/src/Components/Home/Content/UserSettingsPage/UserSettingsPage.tsx
+++ Frontend/src/Components/Home/Content/UserSettingsPage/UserSettingsPage.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import styles from "./UserSettingsPage.module.scss";
 import api from "../../../../ApiConfig/ApiConfig";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Notifications from "../../../Notifications/Notifications";
 
 interface User {
@@ -12,10 +12,13 @@ interface User {
 }
 
 export function UserSettingsPage() {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   const location = useLocation();
+  const userIdLocal = localStorage.getItem("userId");
+  const [showUserEditForm, setShowUserEditForm] = useState<boolean>(
+    location.state?.showUserEditForm || false
+  );
   const [user, setUser] = useState<User>();
-  const [editUser, setEditUser] = useState<boolean>(false);
   const [editUserName, setEditUserName] = useState<string>("");
   const [editUserAge, setEditUserAge] = useState<number | null>(null);
   const [message, setMessage] = useState<string>("");
@@ -29,32 +32,29 @@ export function UserSettingsPage() {
     }, 3000);
   };
 
-  const fetchUser = async () => {
-    const response = await api.get(`/user/${"User"}`);
+  const fetchUser = useCallback(async () => {
+    const response = await api.get(`/userById/${userIdLocal}`);
     setUser(response.data);
 
     setEditUserName(response.data.name);
     setEditUserAge(response.data.age);
-  };
+  }, [userIdLocal]);
 
   useEffect(() => {
     fetchUser();
-  }, []);
+  }, [fetchUser]);
 
   useEffect(() => {
-    if (editUser) {
+    if (showUserEditForm) {
       if (user) {
         setEditUserName(user.name);
         setEditUserAge(user.age);
       }
     }
-  }, [editUser, user]);
+  }, [showUserEditForm, user]);
 
   const handleShowForm = () => {
-    navigate("/settings", {
-      state: { showUserEditForm: location.state?.showUserEditForm !== true },
-    });
-    setEditUser(!editUser);
+    setShowUserEditForm(!showUserEditForm);
   };
 
   const handleEditUser = async (e: React.FormEvent) => {
@@ -64,10 +64,7 @@ export function UserSettingsPage() {
       setEditUserName("");
       setEditUserAge(null);
 
-      navigate("/settings", {
-        state: { showUserEditForm: false },
-      });
-      setEditUser(false);
+      setShowUserEditForm(false);
       fetchUser();
 
       setMessage("User edited successfully!");
@@ -84,64 +81,64 @@ export function UserSettingsPage() {
     <div className={styles.settingsPage}>
       <Notifications messageType={messageType} message={message} />
 
-      <>
-        <div id={styles.formContainer}>
-          <form
-            onSubmit={handleEditUser}
-            className={editUser ? styles.showForm : styles.hideForm}
+      <div id={styles.formContainer}>
+        <form
+          onSubmit={handleEditUser}
+          className={
+            showUserEditForm === true ? styles.showForm : styles.hideForm
+          }
+        >
+          <button
+            className={styles.closeFormBtn}
+            type="button"
+            onClick={() => handleShowForm()}
           >
-            <button
-              className={styles.closeFormBtn}
-              type="button"
-              onClick={() => handleShowForm()}
-            >
-              X
-            </button>
-            <input
-              className={styles.editUserNameInput}
-              type="text"
-              value={editUserName ?? ""}
-              placeholder="Name"
-              onChange={(e) => setEditUserName(e.target.value)}
-              required
-            />
-            <input
-              className={styles.editUserAgeInput}
-              type="text"
-              value={editUserAge !== null ? editUserAge : "--"}
-              placeholder="Age"
-              onChange={(e) => setEditUserAge(Number(e.target.value))}
-            />
-            <button className={styles.submitFormBtn} type="submit">
-              Save
-            </button>
-          </form>
-        </div>
-        {user && (
-          <div key={user.email} className={styles.userCard}>
-            <div className={styles.userAvatar}>
-              <img src={user.avatar} alt={`avatar`} />
-            </div>
-            <div className={styles.userInfo}>
-              <div className={styles.userName}>
-                <strong>{user.name ? user.name : "--"}</strong>
-              </div>
-              <div className={styles.userEmail}>
-                <strong>{user.email}</strong>
-              </div>
-              <div className={styles.userAge}>
-                <strong>{user.age ? user.age : "--"}</strong>
-              </div>
-              <button
-                onClick={() => handleShowForm()}
-                className={styles.userEdit}
-              >
-                Edit profile
-              </button>
-            </div>
+            X
+          </button>
+          <input
+            className={styles.editUserNameInput}
+            type="text"
+            value={editUserName}
+            placeholder="Name"
+            onChange={(e) => setEditUserName(e.target.value)}
+            required
+          />
+          <input
+            className={styles.editUserAgeInput}
+            type="text"
+            value={editUserAge ? editUserAge : ""}
+            placeholder="Age"
+            onChange={(e) => setEditUserAge(Number(e.target.value))}
+          />
+          <button className={styles.submitFormBtn} type="submit">
+            Save
+          </button>
+        </form>
+      </div>
+      {user && (
+        <div key={user.email} className={styles.userCard}>
+          <div className={styles.userAvatar}>
+            <img src={user.avatar} alt={`avatar`} />
           </div>
-        )}
-      </>
+          <div className={styles.userInfo}>
+            <div className={styles.userName}>
+              <strong>{user.name ? user.name : "--"}</strong>
+            </div>
+            <div className={styles.userEmail}>
+              <strong>{user.email}</strong>
+            </div>
+            <div className={styles.userAge}>
+              <strong>{user.age ? user.age : "--"}</strong>
+            </div>
+            <button
+              onClick={() => handleShowForm()}
+              className={styles.userEdit}
+            >
+              Edit profile
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
